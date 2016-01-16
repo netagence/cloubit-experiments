343d1b0c8b5b4a079ef4923304be7297a1061dda
NetAgence <netagence@users.noreply.github.com>
2016-01-16 23:05:09 +0100

    Create test01.sh
    Initial notes, file in progress.

---
+++ test01.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#description     :Experimenting with Littlebits' Cloudbit programming
+#author          :Pierre MOBIAN
+#date            :20160116
+#version         :0.1    
+#usage           :bash test01.sh or ./test01.sh
+
+### SCRIPT CONFIG STARTS HERE ###
+
+DATETIME_MARKER=$(date '+%Y%m%d_%H%M%S')
+LOGFILEPATHNAME="/home/littlebitscloud/$DATETIME_MARKER.log"
+
+clear;
+echo "Littlebits' Cloudbit test"
+echo "Bash version ${BASH_VERSION}"
+
+### DO NOT EDIT BELOW ###
