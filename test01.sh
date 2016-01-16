4e3a8b9df2a3219c33bc8a406c64ac0a3107586b
NetAgence <netagence@users.noreply.github.com>
2016-01-16 23:57:20 +0100

    Update test01.sh

--- test01.sh
+++ test01.sh
@@ -6,8 +6,8 @@
 #usage           :bash test01.sh or ./test01.sh
 
 ### YOUR CLOUDBIT PERSONAL PARAMETERS - GET THOSE FROM YOUR SETTNGS AT http://control.littlebitscloud.cc/ ###
-CLOUDBIT_DEVICEID="xxxxxxxxx""
-CLOUDBIT_ACCESSTOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CLOUDBITDEVICEID="xxxxxxxxx""
+CLOUDBITACCESSTOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 ### SCRIPT CONFIG STARTS HERE - DO NOT EDIT UNLESS YOU KNOW WHAT YOU'RE DOING ###
 DATETIME_MARKER=$(date '+%Y%m%d_%H%M%S')
@@ -17,6 +17,10 @@ clear;
 echo "Littlebits Cloudbit test"
 echo "Bash version ${BASH_VERSION}"
 
-echo "Test running on your Cloudbit with ID $CLOUDBIT_DEVICEID"
- 
+echo "Test running on your Cloudbit with ID $CLOUDBITDEVICEID"
+echo "Basic infos on your device:"
+
+# array=
+curl -i -XGET -H "Authorization: Bearer $CLOUDBITACCESSTOKEN" -H "Accept: application/vnd.littlebits.v2+json" https://api-http.littlebitscloud.cc/devices/$CLOUDBITDEVICEID
+
 ### DO NOT EDIT BELOW ###
