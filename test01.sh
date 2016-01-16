99fd3374393d2344822537cb2dd0469976916734
NetAgence <netagence@users.noreply.github.com>
2016-01-16 23:40:57 +0100

    Update test01.sh

--- test01.sh
+++ test01.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-#description     :Experimenting with Littlebits' Cloudbit programming
+#description     :Experimenting with Littlebits' Cloudbit
 #author          :Pierre MOBIAN
 #date            :20160116
 #version         :0.1    
 #usage           :bash test01.sh or ./test01.sh
 
-### SCRIPT CONFIG STARTS HERE ###
+### YOUR CLOUDBIT PERSONAL PARAMETERS - GET THOSE FROM YOUR SETTNGS AT http://control.littlebitscloud.cc/ ###
+CLOUDBIT_DEVICEID="xxxxxxxxx""
+CLOUDBIT_ACCESSTOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
+### SCRIPT CONFIG STARTS HERE - DO NOT EDIT UNLESS YOU KNOW WHAT YOU'RE DOING ###
 DATETIME_MARKER=$(date '+%Y%m%d_%H%M%S')
-LOGFILEPATHNAME="/home/littlebitscloud/$DATETIME_MARKER.log"
-
-# YOUR CLOUDBIT PERSONAL PARAMETERS - GET THOSE FROM YOUR SETTNGS AT http://control.littlebitscloud.cc/
-CLOUDBIT_DEVICEID=xxxxxxxxx
-CLOUDBIT_ACCESSTOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+LOGFILEPATHNAME="littlebitscloud_$DATETIME_MARKER.log"
 
 clear;
 echo "Littlebits' Cloudbit test"
 echo "Bash version ${BASH_VERSION}"
 
+echo "Test running on your Cloudbit with ID $CLOUDBIT_DEVICEID"
+ 
 ### DO NOT EDIT BELOW ###
